728aa610f7cc3818368c2b493492b0665476ee91
your_username <luca.eich@amazingblocks.io>
2021-05-29 15:32:21 +0100

    First commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,17 @@
+import { MenuEntry } from '@pancakeswap/uikit'
+import { ContextApi } from 'contexts/Localization/types'
+
+const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
+  {
+    label: t('Home'),
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: t('IFO'),
+    icon: 'IfoIcon',
+    href: '/ifo',
+  },
+]
+
+export default config
